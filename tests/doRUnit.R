3a4d04fe435541e4266901065d52f884804834ec
Thomas Wutzler <twutz@bgc-jena.mpg.de>
2018-01-11 12:51:30 +0100

    calcClosedChamberFluxForChunks: error on providing vector instead of data.frame as volumnesByChunk

--- tests/doRUnit.R
+++ tests/doRUnit.R
@@ -1,8 +1,8 @@
-## unit tests will not be done if RUnit is not available
-if(require("testthat", quietly=TRUE)) {
-	#pkg <- "PKG" # <-- Change to package name!
-	pkg <- "RespChamberProc" # <-- Change to package name!
-	test_package(pkg)
+if (require("testthat", quietly = TRUE)) {
+  pkg <- "RespChamberProc" # <-- Change to package name!
+  #library(pkg, character.only = TRUE)
+  #test_package(pkg)
+  test_check(pkg)
 } else {
-	warning("cannot run unit tests -- package testthat is not available")
+  warning("cannot run unit tests -- package testthat is not available")
 }
