4701055ca8d964f2e3a8769da0bdccc3fa6f4ad5
Thomas Wutzler <twutz@bgc-jena.mpg.de>
2018-01-11 16:03:27 +0100

    readDat: explicitely closing connection on fileName

--- tests/testthat/test_readDat.R
+++ tests/testthat/test_readDat.R
@@ -3,7 +3,7 @@ context("readDat")
 
 test_that("x81 example",{
 			fName <- system.file("genData/Flux2_140929_1700.81x", package = "RespChamberProc")
-			if( nzchar(fName) ){
+			if (nzchar(fName)) {
 				ds <- read81x(fName)
 				expect_true( max(ds$iChunk) > 1 )
 				expect_true( table(ds$iChunk)[1] > 1 )
@@ -15,7 +15,7 @@ test_that("x81 example",{
 
 test_that("x81 annotation",{
 			fName <- system.file("genData/testVaryingColNumber.81x", package = "RespChamberProc")
-			if( nzchar(fName) ){
+			if (nzchar(fName)) {
 				ds <- read81x(fName)
 				expect_true( max(ds$iChunk) > 1 )
 				expect_true( table(ds$iChunk)[1] > 1 )
@@ -27,3 +27,41 @@ test_that("x81 annotation",{
 				#plot( CO2 ~ Date, ds[ds$iChunk==9,] )
 			}
 		})
+
+
+test_that("zipped file",{
+  fName <- system.file(
+  "genData/SMANIE_Chamber1_26032015.zip", package = "RespChamberProc")
+  skip_if_not(nzchar(fName))
+  nConn <- nrow(showConnections())
+  con <- unz(fName, filename = unzip(fName, list = TRUE)[1,"Name"] )
+  ds0 <- readDat(con, tz = "UTC")
+  expect_true( nrow(ds0) > 0 )
+  #expect_error( isOpen(con) ) # invalid connection, because closed
+  # number of open connections did not change
+  expect_equal( nConn, nrow(showConnections())) 
+})
+
+test_that("zipped file",{
+  fName <- system.file(
+    "genData/SMANIE_Chamber1_26032015.zip", package = "RespChamberProc")
+  skip_if_not(nzchar(fName))
+  con <- unz(fName, filename = unzip(fName, list = TRUE)[1,"Name"] )
+  open(con)
+  ds0 <- readDat(con, tz = "UTC")
+  expect_true( nrow(ds0) > 0 )
+  expect_true( isOpen(con) ) # invalid connection, because closed
+  close(con)
+})
+
+test_that("reading dat file",{
+  fName <- system.file(
+    "genData/chamberLoggerEx1_short.dat", package = "RespChamberProc")
+  skip_if_not(nzchar(fName))
+  nConn <- nrow(showConnections())
+  ds0 <- readDat(fName, tz = "UTC")
+  expect_true( nrow(ds0) > 0 )
+  # number of open connections did not change
+  expect_equal( nConn, nrow(showConnections())) 
+})
+
