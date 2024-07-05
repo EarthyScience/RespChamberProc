49b53b423b4d1b69008d7efff1934cbde2f9e045
Thomas Wutzler <progtw@arcor.de>
2024-07-05 14:08:03 +0200

    test error message in when specifying small duration uncertainty

--- tests/testthat/test_plotDurationUncertainty.R
+++ tests/testthat/test_plotDurationUncertainty.R
@@ -14,5 +14,21 @@ test_that("run plotDurationUncertainty",{
   expect_that(resDur$duration, equals(120))
 })
 
+test_that("error message when specifying too short duration",{
+  #https://github.com/bgctw/RespChamberProc/issues/8
+  df <- readRDS("inst/genData/issue8_duration.rds")
+  chamberVol = 0.4*0.4*0.4		# define here chamber volume and surface area
+  surfaceArea = 0.4*0.4
+  # reproduces the error
+  expect_error(  
+    resDur_H2O <- plotDurationUncertainty( df
+                                         ,colTime = "TIMESTAMP"
+                                         ,colConc = "H2Oppt"
+                                         ,colTemp="AirTemp"
+                                         ,volume = chamberVol
+                                         , durations = seq(124,404,by=5))
+    ,"requires at least 16 records")
+})
+
 
 			
